674108fe1a75b4ac3e5dda10f784a3d4a13666dd
Hyeon97 <61956880+Hyeon97@users.noreply.github.com>
2025-03-07 07:35:51 +0900

    서버 정보요청 query 옵션 검증 규칙 수정

--- src/api/validators/server.validators.ts
+++ src/api/validators/server.validators.ts
@@ -7,9 +7,11 @@ import { validate } from "../middleware/validationMiddleware"
 //  옵션
 const filterOptionValidationRules = [
   query("os").optional().isIn(["win", "lin"]).withMessage("OS는 'win' 또는 'lin'만 가능합니다."),
-  query("network").optional().isIn(["true", "false"]).withMessage("network는 'true' 또는 'false'만 가능합니다."),
-  query("disk").optional().isIn(["true", "false"]).withMessage("disk는 'true' 또는 'false'만 가능합니다."),
-  query("partition").optional().isIn(["true", "false"]).withMessage("partition은 'true' 또는 'false'만 가능합니다."),
+  query("network").optional().isBoolean().withMessage("network는 'true' 또는 'false'만 가능합니다."),
+  query("disk").optional().isBoolean().withMessage("disk는 'true' 또는 'false'만 가능합니다."),
+  query("partition").optional().isBoolean().withMessage("partition은 'true' 또는 'false'만 가능합니다."),
+  query("repository").optional().isBoolean().withMessage("repository는 'true' 또는 'false'만 가능합니다."),
+  query("detail").optional().isBoolean().withMessage("detail은 'true' 또는 'false'만 가능합니다."),
   query("state").optional().isIn(["connect", "disconnect"]).withMessage("state는 'connect' 또는 'disconnect'만 가능합니다."),
   query("license").optional().isIn(["assign", "unassign"]).withMessage("license는 'assign' 또는 'unassign'만 가능합니다."),
 ]
