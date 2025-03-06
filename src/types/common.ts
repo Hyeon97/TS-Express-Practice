f9766fc12997f0bc9056b23b339463fe858f4b43
Hyeon97 <61956880+Hyeon97@users.noreply.github.com>
2025-03-07 07:33:29 +0900

    불필요한 상수 VALID_BOOLEAN_VALUES 제거거

--- src/types/common.ts
+++ src/types/common.ts
@@ -2,6 +2,5 @@ export type LogFormat = "dev" | "combined" | "common" | "short" | "tiny"
 export type LogLevel = "error" | "warn" | "info" | "http" | "debug" | "silly"
 
 export const VALID_OS_VALUES = ["win", "lin"] as const
-export const VALID_BOOLEAN_VALUES = ["true", "false"] as const
 export const VALID_STATE_VALUES = ["connect", "disconnect"] as const
 export const VALID_LICENSE_VALUES = ["assign", "unassign"] as const
